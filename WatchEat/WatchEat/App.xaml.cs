a6345e658798fe6e17e0ada2bafb2f5ae352b4d7
Roman Łubkowski <roman@lubkowski.net>
2019-08-30 07:04:22 +0200

    App center configuration added.

--- WatchEat/WatchEat/App.xaml.cs
+++ WatchEat/WatchEat/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using WatchEat.Services;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using WatchEat.Services;
 using WatchEat.Services.Interfaces;
 using Xamarin.Forms;
 
@@ -6,7 +9,6 @@ namespace WatchEat
 {
     public partial class App : Application
     {
-
         public App()
         {
             InitializeComponent();
@@ -16,7 +18,7 @@ namespace WatchEat
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            AppCenter.Start("ios=d052f377-ac14-4f09-b175-fea95222709f;android=9d5b0155-9633-438c-b1b1-6beadbd63faf;",typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
