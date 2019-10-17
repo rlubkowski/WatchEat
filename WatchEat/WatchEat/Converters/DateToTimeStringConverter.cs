287ff5aa914e147dcf56697138dac442e3e14615
Roman Łubkowski <roman@lubkowski.net>
2019-10-17 23:31:30 +0200

    Basic journal entry templates.

---
+++ WatchEat/WatchEat/Converters/DateToTimeStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace WatchEat.Converters
+{
+    public class DateToTimeStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime)
+            {
+                return ((DateTime)value).ToString(culture.DateTimeFormat.ShortTimePattern);
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string)
+            {
+                DateTime resultValue;
+                var result = DateTime.TryParse((string)value, culture, DateTimeStyles.None, out resultValue);
+                if (result)
+                {
+                    return resultValue;
+                }
+            }
+            return DateTime.Now;
+        }
+    }
+}
