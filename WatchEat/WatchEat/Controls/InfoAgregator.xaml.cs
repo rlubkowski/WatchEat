cbd0ce0fc79c35decee7cd14135908ffdedeb490
Roman Łubkowski <roman@lubkowski.net>
2019-10-30 05:05:30 +0100

    Work in progress - daily summary.

---
+++ WatchEat/WatchEat/Controls/InfoAgregator.xaml.cs
@@ -0,0 +1,37 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WatchEat.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InfoAgregator : ContentView
+    {
+        public InfoAgregator()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text),
+                                                                                      typeof(string),
+                                                                                      typeof(InfoAgregator),
+                                                                                      string.Empty);
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly BindableProperty ValueProperty = BindableProperty.Create(nameof(Value),
+                                                                                        typeof(decimal),
+                                                                                        typeof(InfoAgregator),
+                                                                                        default(decimal));
+
+        public decimal Value
+        {
+            get { return (decimal)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+    }
+}
---
+++ WatchEat/WatchEat/Converters/ValueToStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace WatchEat.Converters
+{
+    public class ValueToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {            
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
